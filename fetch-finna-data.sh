a6cac255c979ef0336dfc624c0c40a0537ff612a
Jouni Tuominen <jouni.tuominen@aalto.fi>
2017-05-17 00:05:52 +0300

    widen the query's geographic filter to the whole world; get also the license information

--- fetch-finna-data.sh
+++ fetch-finna-data.sh
@@ -8,8 +8,9 @@
 # - author
 # - year
 # - link to image file
+# - image rights
 
-for i in {1..4011};
+for i in {1..4016};
 do
-  curl -o finna-$i.json -X GET --header 'Accept: application/json' 'https://api.finna.fi/api/v1/search?type=AllFields&field%5B%5D=title&field%5B%5D=geoLocations&field%5B%5D=subjects&field%5B%5D=images&field%5B%5D=nonPresenterAuthors&field%5B%5D=year&filter%5B%5D=%7B%21geofilt+sfield%3Dlocation_geo+pt%3D65.01650627048231%2C23.994140625000004+d%3D616.5783931055392%7D%3A%22%22&filter%5B%5D=format%3A%220%2FImage%2F%22&sort=relevance%2Cid%20asc&limit=100&lng=fi&page='$i
+  curl -o finna-$i.json -X GET --header 'Accept: application/json' 'https://api.finna.fi/api/v1/search?field%5B%5D=title&field%5B%5D=geoLocations&field%5B%5D=subjects&field%5B%5D=images&field%5B%5D=nonPresenterAuthors&field%5B%5D=year&field%5B%5D=imageRights&filter%5B%5D=%7B%21geofilt+sfield%3Dlocation_geo+pt%3D65%2C24+d%3D20000%7D%3A%22%22&filter%5B%5D=format%3A%220%2FImage%2F%22&sort=relevance%2Cid%20asc&limit=100&lng=fi&page='$i
 done
