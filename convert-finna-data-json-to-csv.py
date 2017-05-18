6adcc663f11c27ab51ec223c312c50e2f1e9e6df
Jouni Tuominen <jouni.tuominen@aalto.fi>
2017-05-18 17:39:26 +0300

    separate points, polygons and linestrings into different csv files; put point's lat and long into separate fields

--- convert-finna-data-json-to-csv.py
+++ convert-finna-data-json-to-csv.py
@@ -1,34 +1,71 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import codecs
 import json
 import csv
 import os
 
-csvfile = open('finna-2017-05-17.csv', 'w')
-#csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-csvwriter = csv.writer(csvfile)
+POINT = 1
+POLYGON = 2
+LINESTRING = 3
 
-csvwriter.writerow(["title", "geoLocations", "subjects", "images", "nonPresentAuthors", "year", "imageRights"])
+FILE_SUFFIX = "2017-05-18"
 
-for i in range(1,4016):
+csvpointsfile = open('finna-points-' + FILE_SUFFIX + '.csv', 'w')
+csvpolygonsfile = open('finna-polygons-' + FILE_SUFFIX + '.csv', 'w')
+csvlinestringsfile = open('finna-linestrings-' + FILE_SUFFIX + '.csv', 'w')
+
+#TODO: add BOM to file for Excel? (the next line makes everything scramble at least in macOS's Excel 2011)
+#csvpointsfile.write(codecs.BOM_UTF16_LE)
+
+csvpointswriter = csv.writer(csvpointsfile)
+csvpolygonswriter = csv.writer(csvpolygonsfile)
+csvlinestringswriter = csv.writer(csvlinestringsfile)
+
+csvpointswriter.writerow(["title", "lat", "long", "subjects", "images", "nonPresentAuthors", "year", "imageRights"])
+csvpolygonswriter.writerow(["title", "polygon", "subjects", "images", "nonPresentAuthors", "year", "imageRights"])
+csvlinestringswriter.writerow(["title", "linestring", "subjects", "images", "nonPresentAuthors", "year", "imageRights"])
+
+for i in range(1,4017):
     filename = 'finna-data-2017-05-17/finna-'+str(i)+'.json'
     if os.path.isfile(filename):
         jsonfile = open(filename)
         data = json.load(jsonfile)
         jsonfile.close()
 
-        #print filename
-
         for item in data['records']:
             title = ""
             if item.has_key("title"):
-                title = item['title'].encode("utf-8")
+                title = item['title'].encode("utf-8").replace("\n", " ")
 
-            geoLocations = []
+            geoLocation = ""
+            point = ""
+            lat = ""
+            lng = ""
+            polygon = ""
+            linestring = ""
+            geoLocationType = 0
             for geoLocation in item['geoLocations']:
-                geoLocations.append(geoLocation.encode("utf-8"))
-            geoLocations = ", ".join(geoLocations)
+                if geoLocation.startswith("POINT"):
+                    point = geoLocation.encode("utf-8")
+                elif geoLocation.startswith("POLYGON"):
+                    polygon = geoLocation.encode("utf-8")
+                elif geoLocation.startswith("LINESTRING"):
+                    linestring = geoLocation.encode("utf-8")
+            if point != "":
+                point = point.replace("POINT", "").replace("(", "").replace(")", "").strip().split(" ")
+                lat = point[1]
+                lng = point[0]
+                geoLocationType = POINT
+            elif polygon != "":
+                geoLocation = polygon
+                geoLocationType = POLYGON
+            elif linestring != "":
+                geoLocation = linestring
+                geoLocationType = LINESTRING
+            if geoLocation == "":
+                print "Error: no geoLocation: " + filename
 
             subjects = []
             if item.has_key("subjects"):
@@ -56,6 +93,13 @@ for i in range(1,4016):
             if item.has_key("imageRights"):
                 rights = item['imageRights']['copyright'].encode("utf-8")
 
-            csvwriter.writerow([title, geoLocations, subjects, images, authors, year, rights])
+            if geoLocationType == POINT:
+                csvpointswriter.writerow([title, lat, lng, subjects, images, authors, year, rights])
+            elif geoLocationType == POLYGON:
+                csvpolygonswriter.writerow([title, geoLocation, subjects, images, authors, year, rights])
+            elif geoLocationType == LINESTRING:
+                csvlinestringswriter.writerow([title, geoLocation, subjects, images, authors, year, rights])
 
-csvfile.close()
+csvpointsfile.close()
+csvpolygonsfile.close()
+csvlinestringsfile.close()
