e09fb0416618022045ac9754cca86472cb9d59ce
Jouni Tuominen <jouni.tuominen@aalto.fi>
2017-05-18 23:36:59 +0300

    support for filter file, e.g., for selecting only caches near RKY; add error handling

--- geocache-descriptions-parser-lemmatizer.py
+++ geocache-descriptions-parser-lemmatizer.py
@@ -12,7 +12,8 @@ lemmatizer_url = "http://demo.seco.tkk.fi/las/baseform"
 # might be a good idea to use ARPA with "cgen" parameter instead, though adds complexity (n-grams, hard to ensure the word order of the original text)
 
 src_dir = "geocaches/"
-dst_dir = "geocache-descriptions/"
+dst_dir = "geocache-descriptions-2017-05-18/"
+filter_file = "geocaches_names_rky200m.txt"
 
 if os.path.isdir(dst_dir):
     print "Destination dir (" + dst_dir + ") already exists. Please delete it in order not to override previous data."
@@ -20,39 +21,53 @@ if os.path.isdir(dst_dir):
 
 os.mkdir(dst_dir)
 
+if filter_file != "":
+    f = open(filter_file)
+    filter_names = f.readlines()
+    filter_names = [x.strip() for x in filter_names]
+
 for file in os.listdir(src_dir):
     if file.endswith(".gpx"):
         tree = ET.parse(src_dir + file)
         root = tree.getroot()
 
         for cache in root.findall("{http://www.topografix.com/GPX/1/0}wpt"):
+            name = cache.find("{http://www.topografix.com/GPX/1/0}name")
+            if name != None and (filter_file == "" or name.text.encode("utf-8") in filter_names):
+                texts = []
+                cache_inner = cache.find("{http://www.groundspeak.com/cache/1/0/1}cache")
+                if cache_inner != None:
+                    short_description = cache_inner.find("{http://www.groundspeak.com/cache/1/0/1}short_description")
+                    if short_description != None:
+                        texts.append(short_description.text.encode("utf-8"))
+                    long_description = cache_inner.find("{http://www.groundspeak.com/cache/1/0/1}long_description")
+                    if long_description != None:
+                        texts.append(long_description.text.encode("utf-8"))
 
-            texts = []
-            cache_inner = cache.find("{http://www.groundspeak.com/cache/1/0/1}cache")
-            #TODO: it seems that not all caches have descriptions (next line throws exception)
-            texts.append(cache_inner.find("{http://www.groundspeak.com/cache/1/0/1}short_description").text.encode("utf-8"))
-            texts.append(cache_inner.find("{http://www.groundspeak.com/cache/1/0/1}long_description").text.encode("utf-8"))
+                    logs = cache_inner.find("{http://www.groundspeak.com/cache/1/0/1}logs")
+                    if logs != None:
+                        for log in logs.findall("{http://www.groundspeak.com/cache/1/0/1}log"):
+                            logtext = log.find("{http://www.groundspeak.com/cache/1/0/1}text")
+                            if logtext != None:
+                                texts.append(logtext.text.encode("utf-8"))
 
-            for log in cache_inner.find("{http://www.groundspeak.com/cache/1/0/1}logs").findall("{http://www.groundspeak.com/cache/1/0/1}log"):
-                texts.append(log.find("{http://www.groundspeak.com/cache/1/0/1}text").text.encode("utf-8"))
-
-            for text in texts:
-                text = re.sub('<[^<]+?>', '', text)
-                sentences = text.split(".")
-                for sentence in sentences:
-                    sentence = sentence.strip()
-                    if sentence != "":
-                        response = requests.post(lemmatizer_url, data={'text': sentence})
-                        if response.status_code == 200:
-                            try:
-                                resp_json = response.json()
-                            except ValueError:
-                                print "Warning: Non-JSON response."
-                                continue
-                            if resp_json.has_key("locale") and resp_json.has_key("baseform"):
-                                filename = cache.get("lat") + "_" + cache.get("lon") + ".txt"
-                                locale = resp_json['locale']
-                                if not os.path.isdir(dst_dir + locale):
-                                    os.mkdir(dst_dir + locale)
-                                file = open(dst_dir + locale + '/' + filename, 'a')
-                                file.write(resp_json['baseform'].encode("utf-8") + ". ")
+                    for text in texts:
+                        text = re.sub('<[^<]+?>', '', text)
+                        sentences = text.split(".")
+                        for sentence in sentences:
+                            sentence = sentence.strip()
+                            if sentence != "":
+                                response = requests.post(lemmatizer_url, data={'text': sentence})
+                                if response.status_code == 200:
+                                    try:
+                                        resp_json = response.json()
+                                    except ValueError:
+                                        print "Warning: Non-JSON response."
+                                        continue
+                                    if resp_json.has_key("locale") and resp_json.has_key("baseform"):
+                                        filename = cache.get("lat") + "_" + cache.get("lon") + ".txt"
+                                        locale = resp_json['locale']
+                                        if not os.path.isdir(dst_dir + locale):
+                                            os.mkdir(dst_dir + locale)
+                                        file = open(dst_dir + locale + '/' + filename, 'a')
+                                        file.write(resp_json['baseform'].encode("utf-8") + ". ")
