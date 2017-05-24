11480fb62f4e4425f7f361b2d93a2bb8c764b493
Jouni Tuominen <jouni.tuominen@aalto.fi>
2017-05-24 15:30:05 +0300

    add building information to CSV dump

--- convert-finna-data-json-to-csv.py
+++ convert-finna-data-json-to-csv.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import codecs
+#import codecs
 import json
 import csv
 import os
@@ -10,7 +10,7 @@ POINT = 1
 POLYGON = 2
 LINESTRING = 3
 
-FILE_SUFFIX = "2017-05-18"
+FILE_SUFFIX = "2017-05-19"
 
 csvpointsfile = open('finna-points-' + FILE_SUFFIX + '.csv', 'w')
 csvpolygonsfile = open('finna-polygons-' + FILE_SUFFIX + '.csv', 'w')
@@ -23,82 +23,88 @@ csvpointswriter = csv.writer(csvpointsfile)
 csvpolygonswriter = csv.writer(csvpolygonsfile)
 csvlinestringswriter = csv.writer(csvlinestringsfile)
 
-csvpointswriter.writerow(["title", "lat", "long", "subjects", "images", "nonPresentAuthors", "year", "imageRights"])
-csvpolygonswriter.writerow(["title", "polygon", "subjects", "images", "nonPresentAuthors", "year", "imageRights"])
-csvlinestringswriter.writerow(["title", "linestring", "subjects", "images", "nonPresentAuthors", "year", "imageRights"])
+csvpointswriter.writerow(["title", "lat", "long", "subjects", "images", "nonPresentAuthors", "year", "imageRights", "buildings"])
+csvpolygonswriter.writerow(["title", "polygon", "subjects", "images", "nonPresentAuthors", "year", "imageRights", "buildings"])
+csvlinestringswriter.writerow(["title", "linestring", "subjects", "images", "nonPresentAuthors", "year", "imageRights", "buildings"])
 
-for i in range(1,4017):
-    filename = 'finna-data-2017-05-17/finna-'+str(i)+'.json'
-    if os.path.isfile(filename):
-        jsonfile = open(filename)
-        data = json.load(jsonfile)
-        jsonfile.close()
+for file in os.listdir("finna-data-"+FILE_SUFFIX):
+    if file.endswith(".json"):
+        filename = 'finna-data-'+FILE_SUFFIX+'/' + file
+        if os.path.isfile(filename):
+            jsonfile = open(filename)
+            data = json.load(jsonfile)
+            jsonfile.close()
 
-        for item in data['records']:
-            title = ""
-            if item.has_key("title"):
-                title = item['title'].encode("utf-8").replace("\n", " ")
+            for item in data['records']:
+                title = ""
+                if item.has_key("title"):
+                    title = item['title'].encode("utf-8").replace("\n", " ")
 
-            geoLocation = ""
-            point = ""
-            lat = ""
-            lng = ""
-            polygon = ""
-            linestring = ""
-            geoLocationType = 0
-            for geoLocation in item['geoLocations']:
-                if geoLocation.startswith("POINT"):
-                    point = geoLocation.encode("utf-8")
-                elif geoLocation.startswith("POLYGON"):
-                    polygon = geoLocation.encode("utf-8")
-                elif geoLocation.startswith("LINESTRING"):
-                    linestring = geoLocation.encode("utf-8")
-            if point != "":
-                point = point.replace("POINT", "").replace("(", "").replace(")", "").strip().split(" ")
-                lat = point[1]
-                lng = point[0]
-                geoLocationType = POINT
-            elif polygon != "":
-                geoLocation = polygon
-                geoLocationType = POLYGON
-            elif linestring != "":
-                geoLocation = linestring
-                geoLocationType = LINESTRING
-            if geoLocation == "":
-                print "Error: no geoLocation: " + filename
+                geoLocation = ""
+                point = ""
+                lat = ""
+                lng = ""
+                polygon = ""
+                linestring = ""
+                geoLocationType = 0
+                for geoLocation in item['geoLocations']:
+                    if geoLocation.startswith("POINT"):
+                        point = geoLocation.encode("utf-8")
+                    elif geoLocation.startswith("POLYGON"):
+                        polygon = geoLocation.encode("utf-8")
+                    elif geoLocation.startswith("LINESTRING"):
+                        linestring = geoLocation.encode("utf-8")
+                if point != "":
+                    point = point.replace("POINT", "").replace("(", "").replace(")", "").strip().split(" ")
+                    lat = point[1]
+                    lng = point[0]
+                    geoLocationType = POINT
+                elif polygon != "":
+                    geoLocation = polygon
+                    geoLocationType = POLYGON
+                elif linestring != "":
+                    geoLocation = linestring
+                    geoLocationType = LINESTRING
+                if geoLocation == "":
+                    print "Error: no geoLocation: " + filename
 
-            subjects = []
-            if item.has_key("subjects"):
-                for subject in item['subjects']:
-                    subjects.append(subject[0].encode("utf-8"))
-                subjects = ", ".join(subjects)
+                subjects = []
+                if item.has_key("subjects"):
+                    for subject in item['subjects']:
+                        subjects.append(subject[0].encode("utf-8"))
+                    subjects = ", ".join(subjects)
 
-            images = []
-            if item.has_key("images"):
-                for image in item['images']:
-                    images.append("http://finna.fi"+image.encode("utf-8"))
-                images = ", ".join(images)
+                images = []
+                if item.has_key("images"):
+                    for image in item['images']:
+                        images.append("http://finna.fi"+image.encode("utf-8"))
+                    images = ", ".join(images)
 
-            authors = []
-            if item.has_key("nonPresenterAuthors"):
-                for author in item['nonPresenterAuthors']:
-                    authors.append(author['name'].encode("utf-8"))
-            authors = ", ".join(authors)
+                authors = []
+                if item.has_key("nonPresenterAuthors"):
+                    for author in item['nonPresenterAuthors']:
+                        authors.append(author['name'].encode("utf-8"))
+                authors = ", ".join(authors)
 
-            year = ""
-            if item.has_key("year"):
-                year = item['year']
+                year = ""
+                if item.has_key("year"):
+                    year = item['year']
 
-            rights = ""
-            if item.has_key("imageRights"):
-                rights = item['imageRights']['copyright'].encode("utf-8")
+                rights = ""
+                if item.has_key("imageRights"):
+                    rights = item['imageRights']['copyright'].encode("utf-8")
 
-            if geoLocationType == POINT:
-                csvpointswriter.writerow([title, lat, lng, subjects, images, authors, year, rights])
-            elif geoLocationType == POLYGON:
-                csvpolygonswriter.writerow([title, geoLocation, subjects, images, authors, year, rights])
-            elif geoLocationType == LINESTRING:
-                csvlinestringswriter.writerow([title, geoLocation, subjects, images, authors, year, rights])
+                buildings = []
+                for building in item['buildings']:
+                    buildings.append(building['value'].encode("utf-8"))
+                buildings = ", ".join(buildings)
+
+                if geoLocationType == POINT:
+                    csvpointswriter.writerow([title, lat, lng, subjects, images, authors, year, rights, buildings])
+                elif geoLocationType == POLYGON:
+                    csvpolygonswriter.writerow([title, geoLocation, subjects, images, authors, year, rights, buildings])
+                elif geoLocationType == LINESTRING:
+                    csvlinestringswriter.writerow([title, geoLocation, subjects, images, authors, year, rights, buildings])
 
 csvpointsfile.close()
 csvpolygonsfile.close()
